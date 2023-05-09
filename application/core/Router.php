4ba4bf0369f5ac23b7103299ad5911cc85a79ffc
Кирилл Храмов <hramovkirill01@gmail.com>
2023-05-09 12:56:24 +0300

    - Добавили функции добавления, проверки и запуска контроллера.

--- application/core/Router.php
+++ application/core/Router.php
@@ -4,22 +4,46 @@ namespace application\core;
 
 class Router
 {
-    protected $router = [];
+    protected $routes = [];
     protected $params = [];
 
-    public function __construct(){
-
+    public function __construct()
+    {
+        $arr = require 'application/config/routes.php';
+        foreach ($arr as $key => $val) {
+            $this->add($key, $val);
+        }
     }
 
-    public function add(){
-
+    public function add($route, $params)
+    {
+        $route = '#^' . $route . '$#';
+        $this->routes[$route] = $params;
     }
 
-    public function match(){
-
+    public function match()
+    {
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
     }
 
-    public function run(){
-        echo 'start';
+    public function run()
+    {
+        if ($this->match()) {
+            $controller = 'application\controllers\\' . ucfirst($this->params['controller']) . 'Controller.php';
+            if (class_exists($controller)) {
+                echo 'OK';
+            } else {
+                echo 'Не найден: ' . $controller;
+            }
+        } else {
+            echo 'Маршрут не найден !';
+        }
     }
 }
--- application/lib/dev.php
+++ application/lib/dev.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-function debug() {
+function debug($str) {
     echo '<pre>';
     var_dump($str);
     echo '</pre>';
