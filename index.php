1c7d8c8d2fee9e99d087162291573ee498fe786c
Кирилл Храмов <hramovkirill01@gmail.com>
2023-05-09 12:01:16 +0300

    - Добавили автозагрузку классов. - Добавляем класс Роутер

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 
 require 'application/lib/dev.php';
 
-//debug();
+use application\core\Router;
+use application\lib\Db;
 
-echo "Привет !";
+spl_autoload_register(function ($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+    if (file_exists($path)){
+        require $path;
+    }
+});
+
+session_start();
+
+$router = new Router();
+$router->run();
+$db = new Db();
